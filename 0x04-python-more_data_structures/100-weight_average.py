27e00b628110eabe7ed2b9d660bda3a066b5d3bb
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-12 22:22:24 +0100

    Returns the weighted average of all integers tuple (<score>, <weight>)

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def weight_average(my_list=[]):
+    """Returns the weighted average of all integers tuple (<score>, <weight>)"""
+    if not my_list:
+        return 0
+    return sum(list(map(lambda x: x[0] * x[1], my_list))) / sum(
+        list(map(lambda x: x[1], my_list))
+    )
