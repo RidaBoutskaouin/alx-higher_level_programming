8c7ada75c09ba56ff745034df70c10ddd77d777b
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-09-26 20:58:08 +0100

    Private instance attribute using __init__

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""class Square that defines a square by: (based on 0-square.py)
+
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
+    You are not allowed to import any module
+
+"""
+
+
+class Square:
+    """init of a Private instance attribute using init"""
+
+    def __init__(self, size):
+        self.__size = size
