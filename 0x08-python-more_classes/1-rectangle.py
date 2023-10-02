486a540cfce1ccbc472e9c1e44f8dfa99aaa5b0c
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-02 20:46:59 +0100

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+
+""" Module that creates a Rectangle class """
+
+
+class Rectangle:
+    """defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """gets width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """gets height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
