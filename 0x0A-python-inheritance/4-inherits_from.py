716d17eafb3c798671ac792870170ba202c770ab
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-09 22:06:46 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Module that contains a function that returns True if the object
-is an instance of a class that inherited (directly or indirectly)
-from the specified class"""
+"""4-inherits_from"""
 
 
 def inherits_from(obj, a_class):
     """Function that returns True if the object is an instance of a class
     that inherited (directly or indirectly) from the specified class"""
-    if type(obj) is not a_class:
-        return isinstance(obj, a_class)
-    return False
+    if type(obj) is a_class:
+        return False
+    else:
+        return issubclass(type(obj), a_class)
