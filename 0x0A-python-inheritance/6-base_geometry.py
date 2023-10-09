0e4759a962700a7604b3b1a1314a0e7ae0481989
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-09 22:11:24 +0100

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""Module that contains a class BaseGeometry"""
+
+
+class BaseGeometry:
+
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        """Method that raises an Exception with the message area()
+        is not implemented"""
+        raise Exception("area() is not implemented")
