ccab678068a80327c7632414a444c5c588091685
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-09 22:25:31 +0100

    11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Init method"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """String method"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
