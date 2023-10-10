1353fbac0ecb5b67856db938adc67aec6b08c83a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-10 21:05:34 +0100

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write file"""
+
+
+def write_file(filename="", text=""):
+    """Write a text file (UTF8) and prints it to stdout"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        return file.write(text)
+
+
+
