b2bede6c372c1be513b4ce9fa9b864e3dd3aa2a9
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-10 22:09:52 +0100

    Student Class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-"""10-student.py - Defines the Student class"""
+"""9-student.py - Defines the Student class"""
 
 
 class Student:
     """retrieves a dictionary representation of a Student instance"""
 
     def __init__(self, first_name, last_name, age):
-        """initializes a Student instance
-        Args:
-            first_name: first name of student
-            last_name: last name of student
-            age: age of student
-        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=[]):
-        """retrieves a dictionary representation of a Student instance
-        Args:
-            attrs: list of attributes to retrieve
-        """
-        if type(attrs) is not list:
-            return self.__dict__
-        for i in attrs:
-            if type(i) is not str:
-                return self.__dict__
-        return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
-
-    def reload_from_json(self, json):
-        """replaces all attributes of the Student instance
-        Args:
-            json: dictionary of attributes
-        """
-        for i in json:
-            setattr(self, i, json[i])
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+        return self.__dict__
