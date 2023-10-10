83d3e8fe69e99a6ee63622848d99c997b2969349
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-10 21:36:35 +0100

    add args to a list and save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,13 @@ and then save them to a file"""
 
 import sys
 
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+if __name__ == "__main__":
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 try:
     my_list = load_from_json_file("add_item.json")
-except:
+except FileNotFoundError:
     my_list = []
 
 for i in range(1, len(sys.argv)):
