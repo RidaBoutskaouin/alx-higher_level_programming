566e281b8db099759af338b4c5cf02240a276a9a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-10 21:48:39 +0100

    add args to a list and save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ and then save them to a file"""
 import sys
 
 if __name__ == "__main__":
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
     save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 try:
     my_list = load_from_json_file("add_item.json")
