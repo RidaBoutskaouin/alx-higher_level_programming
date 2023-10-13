73be3c12922934fae723c42e994bef3aad1e3ac9
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-13 22:51:03 +0100

    from_json_string(json_string) function for the base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -39,3 +39,11 @@ class Base:
                 for obj in list_objs:
                     list_dicts.append(obj.to_dictionary())
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of JSON string representation json_string"""
+
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
