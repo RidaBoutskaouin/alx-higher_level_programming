782e97724cd62a865703693eea4193cc97726db7
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-13 22:39:39 +0100

    setter for size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -14,3 +14,14 @@ class Square(Rectangle):
     def __str__(self):
         """This method prints the square"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """This method returns the size of a square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """This method sets the size of a square"""
+        self.width = value
+        self.height = value
