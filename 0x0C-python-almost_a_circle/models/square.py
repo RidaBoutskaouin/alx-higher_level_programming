f6328abccf614dc60079c7f00da54874fe89db78
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-10-13 22:40:42 +0100

    update function

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,26 @@ class Square(Rectangle):
         """This method sets the size of a square"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """This method updates the square"""
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                if i == 1:
+                    self.size = args[i]
+                if i == 2:
+                    self.x = args[i]
+                if i == 3:
+                    self.y = args[i]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
