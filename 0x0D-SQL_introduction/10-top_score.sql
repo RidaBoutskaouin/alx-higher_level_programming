3871363b042125d08468cb3fc54c5a58b4195e2c
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-11-14 21:49:42 +0100

    ORDER BYY

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECR `score`, `name`
-FROM `second_table`
+-- Results are ordered by score in descending order.
+SELECR `score`, `name` FROM `second_table`
 ORDER BY `score` DESC;
