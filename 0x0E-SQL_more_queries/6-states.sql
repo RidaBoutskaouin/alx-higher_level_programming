d13fb6f0705f6fb83f3d9af817f3c46208002434
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-11-15 21:20:51 +0100

    database hbtn_0d_usa

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
--- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
+CREATE TABLE IF NOT EXISTS states (
 	id INT NOT NULL AUTO_INCREMENT UNIQUE,
 	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY(id)
