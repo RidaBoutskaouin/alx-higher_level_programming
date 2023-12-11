5585ced997516ead548e72672e93e040152f6746
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-12-11 22:21:49 +0100

    sort

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 // script that searches the second biggest integer in the list of arguments.
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
-}
+process.argv.length <= 3 ? console.log(0) : console.log(process.argv.sort((a, b) => a - b)[process.argv.length - 2]);
