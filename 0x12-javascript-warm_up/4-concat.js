b4617b671c6c10a35e895679615ca72550348582
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-12-11 21:05:08 +0100

    concat

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // concat 2 arguments
 
-if (process.argv[2] && process.argv[3]) {
-	console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
