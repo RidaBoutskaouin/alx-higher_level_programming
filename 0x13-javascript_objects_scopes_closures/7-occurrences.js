c46f561b98cf17c5742d9448a160812f5a39cbe4
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-12-12 20:02:10 +0100

    semi

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,9 +3,9 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
   for (const item of list) {
-	if (item === searchElement) {
-	  count++;
-	}
+    if (item === searchElement) {
+      count++;
+    }
   }
   return count;
 };
