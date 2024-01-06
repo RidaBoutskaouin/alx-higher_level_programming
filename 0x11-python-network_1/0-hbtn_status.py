682c149076cbda1e679ebaabbd635bbc4cf9fa86
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-07 00:51:44 +0100

    Fix import statement and variable name in 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status"""
 
-from urllib.request import Request, urlopen
-
 if __name__ == "__main__":
+    from urllib.request import Request, urlopen
     req = Request("https://intranet.hbtn.io/status")
-    with urlopen(req) as r:
-        page = r.read()
+    with urlopen(req) as res:
+        page = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(page)))
         print("\t- content: {}".format(page))
