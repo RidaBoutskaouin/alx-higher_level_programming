e47f55c11f43c5c8c56db1083d71759a9392ce29
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-07 12:44:17 +0100

    fetch status using requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" What's my status? #1 using requests module """
+
+if __name__ == "__main__":
+    import requests
+
+    url = 'https://intranet.hbtn.io/status'
+    response = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
