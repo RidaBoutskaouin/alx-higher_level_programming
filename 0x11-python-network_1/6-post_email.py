02c9d6305f4872f2c9592b36c004d0c59f806e1a
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-08 00:37:14 +0100

    \n

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     from sys import argv
 
     url = argv[1]
-    email = {'email': argv[2]}
+    email = {"email": argv[2]}
     response = requests.post(url, data=email)
     print(response.text)
