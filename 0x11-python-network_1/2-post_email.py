3a677a6c77559ef09526037389353ce2df00e6eb
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-07 12:31:32 +0100

    POST

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email address, sends a POST request to the passed URL
-with the email as a parameter, and displays the body of the response (decoded
-in utf-8)."""
+"""
+POST script
+"""
+
 
 if __name__ == "__main__":
-    """Don't execute if imported"""
-    from sys import argv
     from urllib.request import Request, urlopen
-    from urllib.parse import urlencode
+    from sys import argv
+    import urllib.parse
 
     url = argv[1]
-    email = urlencode({"email": argv[2]}).encode("ascii")
+    values = {'email': argv[2]}
+    data = urllib.parse.urlencode(values)
+    email = data.encode('ascii')
     req = Request(url, email)
-    with urlopen(req) as res:
-        print(res.read().decode("utf8"))
+    with urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf8'))
