b476ae78c85e7d5e14f90fe7951c2cbb8420837c
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-08 00:28:13 +0100

    request post

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/share/python3
+""" 6. POST an email #1 """
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    email = {'email': argv[2]}
+    response = requests.post(url, data=email)
+    print(response.text)
