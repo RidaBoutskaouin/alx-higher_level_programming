beb38e465186c9c599630bfff79ef713a4af6165
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-07 12:54:12 +0100

    Update URL in 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     import requests
 
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     response = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
