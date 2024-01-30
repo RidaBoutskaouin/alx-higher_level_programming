6786947d5d0b59b6e82527d5308e20c7fd33ce88
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-30 18:04:31 +0100

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@ const file = process.argv[2];
 
 fs.readFile(file, 'utf8', function (err, data) {
   if (err) {
-	return console.log(err);
+    return console.log(err);
   }
   console.log(data);
 });
