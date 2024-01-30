a5746c0f0eb03643e686bdb29daaf0eb3dec066b
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2024-01-30 20:26:08 +0100

    semistabdard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,8 +6,8 @@ const url = process.argv[2];
 
 request(url, function (error, response) {
   if (error) {
-	console.log(error);
+    console.log(error);
   } else {
-	console.log('code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
